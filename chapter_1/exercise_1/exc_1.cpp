dcc3e82b845f675dddb7099fa79b129f30d76613
kirinsgirl <nahdahhanifah@proton.email>
2026-02-25 16:21:12 +0700

    exercise_1

---
+++ chapter_1/exercise_1/exc_1.cpp
@@ -0,0 +1,16 @@
+/*
+    1. Write a C++ program that display a name and adress
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Joel Miller" 
+         << endl 
+         << "Jackson" 
+         << endl;
+
+    return 0;
+}
